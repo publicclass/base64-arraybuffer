4196f51e358cced85d51ab2613f917cb80979885
Robert Sköld <robert@publicclass.se>
2013-01-19 14:54:03 +0100

    Rewrote the tests to use mocha & expect.

---
+++ Makefile
@@ -0,0 +1,13 @@
+test: build
+	./node_modules/.bin/mocha test.js
+
+build: components index.js
+	@component build --dev
+
+components: component.json
+	@component install --dev
+
+clean:
+	rm -fr build components template.js
+
+.PHONY: clean
---
+++ test.js
@@ -0,0 +1,84 @@
+var base64 = require('./index')
+  , expect = require('expect.js');
+
+function buf(str){
+  var buffer = new ArrayBuffer(str.length);
+  var bytes = new Uint8Array(buffer);
+  for(var i=0; i < str.length; i++)
+    bytes[i] = str.charCodeAt(i);
+  return buffer;
+}
+
+function str(buf){
+  var string = '';
+  var bytes = new Uint8Array(buf);
+  for(var i=0; i < bytes.length; i++)
+    string += String.fromCharCode(bytes[i]);
+  return string;
+}
+
+describe('base64-arraybuffer',function(){
+
+  describe('helpers',function(){
+
+    it('buf > str',function(){
+      expect(buf('abc')).to.eql(buf(str(buf('abc'))))
+    })
+
+    it('str > buf',function(){
+      expect('123').to.eql(str(buf('123')))
+    })
+
+  })
+
+  describe('encode',function(){
+
+    it('empty buffer',function(){
+      expect(base64.encode(new ArrayBuffer(0))).to.equal('')
+    })
+
+    it('Hello world',function(){
+      expect(base64.encode(buf('Hello world'))).to.equal('SGVsbG8gd29ybGQ=')
+    })
+
+    it('Man',function(){
+      expect(base64.encode(buf('Man'))).to.equal('TWFu')
+    })
+
+    it('Ma',function(){
+      expect(base64.encode(buf('Ma'))).to.equal('TWE=')
+    })
+
+    it('Hello worlds!',function(){
+      expect(base64.encode(buf('Hello worlds!'))).to.equal('SGVsbG8gd29ybGRzIQ==')
+    })
+
+  })
+
+
+  describe('decode',function(){
+
+    it('empty string',function(){
+      expect(base64.decode('')).to.eql(new ArrayBuffer(0))
+    })
+
+    it('Hello world',function(){
+      expect(base64.decode('SGVsbG8gd29ybGQ=')).to.eql(buf('Hello world'))
+    })
+
+    it('Man',function(){
+      expect(base64.decode('TWFu')).to.eql(buf('Man'))
+    })
+
+    it('Ma',function(){
+      expect(base64.decode('TWE=')).to.eql(buf('Ma'))
+    })
+
+    it('Hello worlds!',function(){
+      expect(base64.decode('SGVsbG8gd29ybGRzIQ==')).to.eql(buf('Hello worlds!'))
+    })
+
+
+  })
+
+})
